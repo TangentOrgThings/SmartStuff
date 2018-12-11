8e67b5f5d686eb46608bde01123b498a42dd8152
Brian Aker <brian@tangent.org>
2018-12-11 01:29:27 -0800

    Update

--- devicetypes/tangentorgthings/ecolink-motion-detector.src/ecolink-motion-detector.groovy
+++ devicetypes/tangentorgthings/ecolink-motion-detector.src/ecolink-motion-detector.groovy
@@ -448,14 +448,12 @@ def zwaveEvent(zwave.commands.notificationv3.NotificationReport cmd, result) {
         return
     }
   } else if (cmd.notificationType == 8) {
-    logger("unknown notificationType: ${cmd}", "warn")
     result << createEvent(name: "battery", unit: "%", value: cmd.event == 0xFF ? 1 : 90, descriptionText: "Battery level", isStateChange: did_batterylevel_change)
   } else if (cmd.notificationType) {
     logger("unknown event for notificationType: ${cmd}", "error")
   } else {
     def value = cmd.v1AlarmLevel == 255 ? "active" : cmd.v1AlarmLevel ?: "inactive"
-    logger("unknown event for v1AlarmType: ${cmd}", "error")
-    // result << createEvent(name: "alarm $cmd.v1AlarmType", value: value, isStateChange: true, displayed: true)
+    sensorValueEvent((Boolean)value, result)
   }
 }
 
@@ -653,9 +651,9 @@ def zwaveEvent(zwave.commands.configurationv1.ConfigurationReport cmd, result) {
   Boolean _isConfigured = false
 
   if (cmd.parameterNumber == 0x63) {
-    if ( cmd.configurationValue.length && cmd.configurationValue[0] == 0x00 ) {
+    if ( cmd.configurationValue && cmd.configurationValue[0] == 0x00 ) {
       result << createEvent(name: "Basic Report", value: "Off", displayed: true)
-    } else if ( cmd.configurationValue.length && cmd.configurationValue[0] == 0xFF ) {
+    } else if ( cmd.configurationValue && cmd.configurationValue[0] == 0xFF ) {
       result << createEvent(name: "Basic Report", value: "On", displayed: true)
       _isConfigured = true
     } else { // Unknown state
@@ -685,43 +683,33 @@ def zwaveEvent(zwave.commands.configurationv2.ConfigurationReport cmd, result) {
   Boolean _isConfigured = false
 
   if (cmd.parameterNumber == 0x63) {
-    if ( cmd.configurationValue.length && cmd.configurationValue[0] == 0x00 ) {
+    if ( cmd.configurationValue && cmd.configurationValue[0] == 0x00 ) {
       result << createEvent(name: "Basic Report", value: "Off", displayed: true)
-    } else if ( cmd.configurationValue.length && cmd.configurationValue[0] == 0xFF ) {
+    } else if ( cmd.configurationValue && cmd.configurationValue[0] == 0xFF ) {
       result << createEvent(name: "Basic Report", value: "On", displayed: true)
       _isConfigured = true
     } else { // Unknown state
     }
   } else if (cmd.parameterNumber == 0x01) {
-  / *
-      0x00 (Default) Sensor does NOT send Basic Sets to Node IDs in Association Group 2 when the sensor is restored (i.e. Motion Not Detected ).
-      0xFF Sensor sends Basic Sets of 0x00 to nodes in Association Group2 when sensor is restored.
-    */
-    result << createEvent(name: "Basic Set Off", value: cmd.configurationValue[0] == 0xFF ? "On" : "Off", displayed: true)
-    if (0) { // Followup off
-      if ( cmd.configurationValue[0] == 0x00 ) {
-        result << response(delayBetween([
-        zwave.configurationV1.configurationSet(parameterNumber: cmd.parameterNumber, configurationValue: [255], size: 1).format(),
-        zwave.configurationV1.configurationGet(parameterNumber: cmd.parameterNumber).format(),
-        ], 800))
-      }
+    // 0x00 (Default) Sensor does NOT send Basic Sets to Node IDs in Association Group 2 when the sensor is restored (i.e. Motion Not Detected ).
+    // 0xFF Sensor sends Basic Sets of 0x00 to nodes in Association Group2 when sensor is restored.
+
+    if ( cmd.configurationValue && cmd.configurationValue[0] == 0xFF ) {
+      result << createEvent(name: "Basic Set Off", value: "On", displayed: true)
+    } else {
+      result << createEvent(name: "Basic Set Off", value:"Off", displayed: true)
     }
+
     _isConfigured = true
   } else if (cmd.parameterNumber == 0x02) {
-  /*
-      0x00 (Default) Sensor sends Sensor Binary Reports when sensor is faulted and restored for backwards compatibility in addition to Notification Reports.
-      0xFF Sensor will send only Notification Reports and NOT Sensor Binary Reports when the sensor is faulted and restored.
-   */
-    result << createEvent(name: "Lifeline Sensor Binary Report", value: cmd.configurationValue[0] == 0xFF ? "Off" : "On", displayed: true);
-
-    if (0) { // Disable default setting of...
-      if ( cmd.configurationValue[0] == 0x00 ) {
-        result << response(delayBetween([
-        zwave.configurationV1.configurationSet(parameterNumber: cmd.parameterNumber, configurationValue: [255], size: 1).format(),
-        zwave.configurationV1.configurationGet(parameterNumber: cmd.parameterNumber).format(),
-        ], 800))
-      }
+    // 0x00 (Default) Sensor sends Sensor Binary Reports when sensor is faulted and restored for backwards compatibility in addition to Notification Reports.
+    // 0xFF Sensor will send only Notification Reports and NOT Sensor Binary Reports when the sensor is faulted and restored.
+    if ( cmd.configurationValue && cmd.configurationValue[0] == 0xFF ) {
+      result << createEvent(name: "Lifeline Sensor Binary Report", value: "On", displayed: true);
+    } else {
+      result << createEvent(name: "Lifeline Sensor Binary Report", value: "Off", displayed: true)
     }
+
     _isConfigured = true
   } else {
     logger("Unknown parameterNumber number ${cmd.parameterNumber}", "error")
