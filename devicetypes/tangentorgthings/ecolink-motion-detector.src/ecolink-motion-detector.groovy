6d571aa2711b2068fd4457999a3b2f1495aadc22
Brian Aker <brian@tangent.org>
2018-12-10 22:53:50 -0800

    Fix Duplicate

--- devicetypes/tangentorgthings/ecolink-motion-detector.src/ecolink-motion-detector.groovy
+++ devicetypes/tangentorgthings/ecolink-motion-detector.src/ecolink-motion-detector.groovy
@@ -556,25 +556,6 @@ def zwaveEvent(zwave.commands.manufacturerspecificv2.DeviceSpecificReport cmd, r
   }
 }
 
-def zwaveEvent(zwave.commands.manufacturerspecificv2.DeviceSpecificReport cmd, result) {
-  logger("$cmd")
-
-  updateDataValue("deviceIdData", "${cmd.deviceIdData}")
-  updateDataValue("deviceIdDataFormat", "${cmd.deviceIdDataFormat}")
-  updateDataValue("deviceIdDataLengthIndicator", "${cmd.deviceIdDataLengthIndicator}")
-  updateDataValue("deviceIdType", "${cmd.deviceIdType}")
-
-  if (cmd.deviceIdType == 1 && cmd.deviceIdDataFormat == 1) {//serial number in binary format
-    String serialNumber = "h'"
-
-    cmd.deviceIdData.each{ data ->
-      serialNumber += "${String.format("%02X", data)}"
-    }
-
-    updateDataValue("serialNumber", serialNumber)
-  }
-}
-
 def zwaveEvent(zwave.commands.manufacturerspecificv2.ManufacturerSpecificReport cmd, result) {
   logger("$cmd")
 
